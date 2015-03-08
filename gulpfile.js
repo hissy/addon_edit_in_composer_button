5b741991d3283da58dae5ed4dc6426f0bba808f4
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2015-03-08 22:45:29 +0900

    Add build task

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+
+gulp.task('zip', function () {
+    return gulp.src(['edit_in_composer_button/**/*'], {base: "./edit_in_composer_button"})
+        .pipe(zip('edit_in_composer_button.zip'))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['zip']);
